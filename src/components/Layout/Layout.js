a9de170c1a17cca49a9244d49eb3ab095b129ffd
Prajan Acharya <prajanacharya11@gmail.com>
2020-06-06 18:09:01 +0545

    Addition of toolbar for mobile devices and some updates in css for responsive designs

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,11 +2,13 @@ import React from "react";
 import Aux from "../../hoc/Auxilliary.js";
 import classes from "./Layout.css";
 import Toolbar from "../Navigation/Toolbar/Toolbar.js";
+import SideDrawer from "../Navigation/SideDrawer/SideDrawer.js";
 
 const Layout = (props) => (
   <Aux>
     {/* <div>Toolbar, Sidedrawer, Backdrop</div> */}
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>{props.children}</main>
   </Aux>
 );
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,20 +1,21 @@
 import React from "react";
-import classes from "./Toolbar.css";
 import Logo from "../../Logo/Logo.js";
 import NavigationItems from "../NavigationItems/NavigationItems.js";
+import classes from "./SideDrawer.css";
 
-const toolbar = (props) => {
+const sideDrawer = (props) => {
+  //....
   return (
-    <header className={classes.Toolbar}>
-      <div>MENU</div>
+    <div className={classes.SideDrawer}>
       <div className={classes.Logo}>
         <Logo />
       </div>
-      <nav className={classes.DesktopOnly}>
+
+      <nav>
         <NavigationItems />
       </nav>
-    </header>
+    </div>
   );
 };
 
-export default toolbar;
+export default sideDrawer;
