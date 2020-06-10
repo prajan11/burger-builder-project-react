933c2f5c42de1ef796e644ff3f4f94720756b20a
Prajan Acharya <prajanacharya11@gmail.com>
2020-06-10 09:31:35 +0545

    Checkout Page Development + React Router Dom package installe

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Layout from "./hoc/Layout/Layout.js";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder.js";
+import Checkout from "./containers/Checkout/Checkout.js";
 
 function App() {
   return (
     <div>
       <Layout>
         <BurgerBuilder />
+        <Checkout />
       </Layout>
     </div>
   );
