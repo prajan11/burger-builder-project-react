a1d97965b4cd77d65403f8a550df3f6cfed69bd5
Prajan Acharya <prajanacharya11@gmail.com>
2020-06-10 15:17:17 +0545

    Loaded all the orders in orders page

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -4,9 +4,27 @@ import classes from "./Order.css";
 const order = (props) => {
   return (
     <div className={classes.Order}>
-      <p>Ingredients: salad(1)</p>
       <p>
-        Price: <strong>$5</strong>
+        Ingredients:
+        {Object.keys(props.ingredients).map((ingredient) => {
+          return (
+            <span
+              key={ingredient}
+              style={{
+                textTransform: "capitalize",
+                display: "inline-block",
+                margin: "0 8px",
+                border: "1px solid black",
+                padding: "5px",
+              }}
+            >
+              {ingredient + props.ingredients[ingredient]}
+            </span>
+          );
+        })}
+      </p>
+      <p>
+        Price: <strong>${props.price}</strong>
       </p>
     </div>
   );
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,16 +1,42 @@
 import React, { Component } from "react";
 import Order from "../../components/Order/Order.js";
+import axios from "../../axios-orders.js";
+import withError from "../../hoc/withErrorHandler/withErrorHandler.js";
 
 class Orders extends Component {
-  state = {};
+  state = { orders: [], loading: false };
+  componentDidMount() {
+    axios
+      .get("/orders.json")
+      .then((res) => {
+        const fetchedOrders = [];
+        for (let key in res.data) {
+          fetchedOrders.push({
+            ...res.data[key],
+            id: key,
+          });
+        }
+        this.setState({ loading: false, orders: fetchedOrders });
+      })
+      .catch((err) => {
+        this.setState({ loading: false });
+      });
+  }
   render() {
     return (
       <div>
-        <Order />
-        <Order />
+        {this.state.orders.map((order) => {
+          return (
+            <Order
+              key={order.id}
+              ingredients={order.ingredients}
+              price={order.price}
+            />
+          );
+        })}
       </div>
     );
   }
 }
 
-export default Orders;
+export default withError(Orders, axios);
